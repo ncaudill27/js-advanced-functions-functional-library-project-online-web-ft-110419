f2b6f063cc6de5bcc6f5601433e34e997e9a0a23
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-07 23:05:51 -0500

    Done.

--- index.js
+++ index.js
@@ -5,20 +5,31 @@ const fi = (function() {
     },
 
     each: function(collection, callback) {
-      if ( Array.isArray(collection) {
-        console.log("array")
-      } else {
-        console.log("object")
-      }
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+
+      for (let idx = 0; idx < newCollection.length; idx++)
+        callback(newCollection[idx])
+
       return collection
     },
 
-    map: function() {
+    map: function(coll, func) {
+      const newColl = (coll instanceof Array) ? coll.slice() : Object.values(coll)
+      const mutatedColl = []
 
+      for ( let i = 0; i < newColl.length; i++ ) {
+        mutatedColl.push(func(newColl[i]))
+      }
+      return mutatedColl
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      const newColl = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      let total = acc
+      for ( let i = 0; i < newColl.length; i++ ) {
+        total = callback(total, newColl[i], newColl)
+      }
+      return total
     },
 
     functions: function() {
