f156dfe85022c6185757652edfc021132ee85e1b
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-08 11:54:58 -0400

    Done.

--- index.js
+++ index.js
@@ -37,17 +37,60 @@ const fi = (function() {
       return acc
     },
 
-    find: function(arr, cb = () => {}) {
-      if (!(Array.isArray(arr))) {
-        arr = Object.values(arr)
-      }      
+    find: function(collection, predicate) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
 
-      for ( val in arr )
-        if cb(arr[val] return arr[val]
+      for (let idx = 0; idx < collection.length; idx++)
+        if (predicate(collection[idx])) return collection[idx]
 
       return undefined
     },
 
+    filter: function(collection, cb) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+
+      let confirmed = []
+
+      for (let idx = 0; idx < collection.length; idx++)
+        if (cb(collection[idx])) confirmed.push(collection[idx])
+
+      if (!confirmed.length) return undefined
+      return confirmed
+    },
+
+    size: function(collection) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+      
+      return collection.length
+    },
+
+    first: function(collection, end) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+      
+      return end ? collection.slice(0, end) : collection[0]
+    },
+
+    last: function(collection, amt) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+
+      return amt ? collection.slice(-(amt)) : parseInt(collection.slice(-1), 10)
+    },
+
+    compact: function(arr) {
+      let keepers = []
+      arr = arr.slice()
+
+      for ( let i = 0; i < arr.length; i++ ) {
+        if (arr[i] != false) keepers.push(arr[i])
+      }
+      return keepers
+    },
+    
     functions: function() {
 
     },
